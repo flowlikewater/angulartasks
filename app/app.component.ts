5303a72564d8fe4ca22101a9ed9d95ed86611572
Kevin <cho.kevin@hotmail.com>
2017-02-21 17:42:52 +0800

    final commit

---
+++ app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { Task } from './task.model';
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <div class="container">
+    <h1>My First Angular 2 App</h1>
+    <pies></pies>
+    <new-task
+      (newTaskSender)="addTask($event)"
+    ></new-task>
+    <task-list
+      [childTaskList]="masterTaskList"
+      (clickSender)="showDetails()"
+     ></task-list>
+    <edit-task
+      [childSelectedTask]="selectedTask"
+      (doneClickedSender)="finishedEditing()"
+    ></edit-task>
+  </div>
+  `
+})
+
+export class AppComponent {
+  public masterTaskList: Task[] = [
+      new Task("Create To-Do List app.", 0),
+      new Task("Learn Kung Fu.", 1),
+      new Task("Rewatch all the Lord of the Rings movies.", 2),
+      new Task("Do the laundry.", 3)
+  ];
+  selectedTask: Task = null;
+  showDetails(clickedTask: Task) {
+    this.selectedTask = clickedTask;
+  }
+  finishedEditing() {
+    this.selectedTask = null;
+  }
+  addTask(addedTask: Task) {
+    this.masterTaskList.push(addedTask);
+  }
+}
